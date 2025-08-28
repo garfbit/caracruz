6ed1d66529c0cb12787630f1811776d0e22e7559
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 06:48:25 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    if (Math.randomBoolean()) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
